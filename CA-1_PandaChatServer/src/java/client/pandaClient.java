e056db74813b2bffb558018a4f46628853e40cf9
Mikkel <Mikkel@10.50.132.19>
2015-09-08 14:03:03 +0200

    Gui er lavet og virker

--- CA-1_PandaChatServer/src/java/client/pandaClient.java
+++ CA-1_PandaChatServer/src/java/client/pandaClient.java
@@ -5,10 +5,63 @@
  */
 package client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Observable;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sharedprotocol.PandaProtocol;
+
 /**
  *
  * @author Mikkel
  */
-public class pandaClient {
+public class pandaClient extends Observable{
     
+    Socket socket;
+    private int port;
+    private InetAddress serverAddress;
+    private Scanner input;
+    private PrintWriter output;
+    
+    
+    public void connect(String address, int port) throws UnknownHostException, IOException {
+        this.port = port;
+        serverAddress = InetAddress.getByName(address);
+        socket = new Socket(serverAddress, port);
+        input = new Scanner(socket.getInputStream());
+        output = new PrintWriter(socket.getOutputStream(), true); //Set to true, to get auto flush behaviour
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                
+                while(true){
+                String msg = input.nextLine();
+                setChanged();
+                notifyObservers(msg);
+
+                if (msg.equals(PandaProtocol.STOP)) {
+                    try {
+                        socket.close();
+                    } catch (IOException ex) {
+                        Logger.getLogger(pandaClient.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            }
+        }).start();
+    }
+    
+    public void send(String msg) {
+        output.println(msg);
+    }
+
+    public void stop() throws IOException {
+        output.println(PandaProtocol.STOP);
+    }
 }
--- CA-1_PandaChatServer/src/java/gui/pandaGUI.java
+++ CA-1_PandaChatServer/src/java/gui/pandaGUI.java
@@ -5,17 +5,34 @@
  */
 package gui;
 
+import client.pandaClient;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.Utils;
+
 /**
  *
  * @author Mikkel
  */
 public class pandaGUI extends javax.swing.JFrame {
 
-    /**
-     * Creates new form pandaGUI
-     */
+    pandaClient panda;
+    private static final Properties properties = Utils.initProperties("pandaProperty.properties");
+    
     public pandaGUI() {
         initComponents();
+        int port = Integer.parseInt(properties.getProperty("port"));
+        String ip = properties.getProperty("serverIp");
+        panda = new pandaClient();
+        try {
+            panda.connect(ip, port);
+        } catch (IOException ex) {
+            Logger.getLogger(pandaGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -27,21 +44,86 @@ public class pandaGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        inputField = new javax.swing.JTextField();
+        sendButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        outputArea = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        inputField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inputFieldActionPerformed(evt);
+            }
+        });
+
+        sendButton.setText("Send");
+        sendButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("PandaChat!!!");
+
+        outputArea.setColumns(20);
+        outputArea.setRows(5);
+        jScrollPane1.setViewportView(outputArea);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(inputField, javax.swing.GroupLayout.PREFERRED_SIZE, 273, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(18, 18, 18)
+                            .addComponent(sendButton))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(18, 18, 18)
+                            .addComponent(jLabel1))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(inputField, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(sendButton))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 356, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(33, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void inputFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputFieldActionPerformed
+
+    private void sendButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendButtonActionPerformed
+        panda.send(inputField.getText());
+        
+        panda.addObserver(new Observer() {
+
+                @Override
+                public void update(Observable o, Object arg) {
+                    
+                    outputArea.setText(arg.toString());
+                }
+            });
+    }//GEN-LAST:event_sendButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +161,10 @@ public class pandaGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField inputField;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea outputArea;
+    private javax.swing.JButton sendButton;
     // End of variables declaration//GEN-END:variables
 }
